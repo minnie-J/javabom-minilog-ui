90e299efa6884b4fb1c3f47927aa77487695954c
MinHee Jeong <38336567+minnie-J@users.noreply.github.com>
2019-07-23 01:43:37 +0900

    [Page #1] Main Page 레이아웃 구상 (#2)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
-import { withErrorBoundary } from "./hocs";
+// import { withErrorBoundary } from "./hocs";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux"
+import {withRouter} from "react-router-dom"
+
+import {init} from "./ducks/modules/articles"
 
 import AppPresenter from "./app.presenter";
 
-const App = withErrorBoundary(AppPresenter);
+const mapDispatchToProps = dispatch => ({
+  initArticlesReducer: bindActionCreators(init, dispatch)
+})
+
+// const App = withErrorBoundary(AppPresenter);
+
+const App = withRouter(
+  connect(
+    null, 
+    mapDispatchToProps
+  )(AppPresenter)
+)
 
 export default App;
--- src/app.presenter.js
+++ src/app.presenter.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {Route, Switch, Redirect} from "react-router-dom"
 
-const App = () => <div>Hello React</div>;
+import {PAGES} from "./assets/js/consts"
+
+import MainPage from "./components/pages/main-page/main-page"
+
+// const App = () => <div>Hello React</div>;
+
+const App = ({initArticlesReducer}) => {
+  useEffect(() => {
+    initArticlesReducer();
+  }, []);
+
+  return (
+    <Switch>
+      <Route path={PAGES.byId.ARTICLES.path} component={MainPage} />
+      {/* <Route path={PAGES.byId.DASHBOARD.path} component={DashboardPage} /> */}
+      <Route path="/" render={() => <Redirect to={PAGES.byId.ARTICLES.path}/>}/>
+    </Switch>
+  )
+}
 
 export default App;
