64367dd0243c5e3300cb012df65d7013ce3e93ce
minnie-J <minhee.jeong@brique.co.kr>
2019-07-28 21:32:10 +0900

    폰트 추가

--- src/global-style.js
+++ src/global-style.js
@@ -5,8 +5,14 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
 
   html, body, #root {
+    @import url('https://fonts.googleapis.com/css?family=Antic&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Inconsolata&display=swap');
     height: 100%;
   }
+
+  /* main {
+    padding-top: 77px;
+  } */
 `;
 
 export default GlobalStyle;
