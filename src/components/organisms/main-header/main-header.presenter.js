d06ab3e48f1ac994636a4369b24384d319b56ef6
minnie-J <minhee.jeong@brique.co.kr>
2019-07-30 02:50:18 +0900

    카테고리 태그 추가

--- src/components/organisms/main-header/main-header.presenter.js
+++ src/components/organisms/main-header/main-header.presenter.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import styled from "styled-components"
-import { Icon, Collapse } from 'antd';
+import { Icon, Collapse, Tag } from 'antd';
 import "./mod-antd.css";
 
 const { Panel } = Collapse;
+const {CheckableTag} = Tag;
 
 const Container = styled.div`
   display: flex;
@@ -74,9 +75,20 @@ const CategoryArea = styled.div`
   align-items: center;
 `;
 
+const tempCat = [
+  "ALL", "영화", "맛집", "여행", "책", 
+  "Develop", "일상", "Blu-ray", 
+  "갖고싶다", "꿀팁", "드라마", 
+  "예능", "아무거나", "또뭐없나", "카테고리늘리기", 
+  "힘드네", "생각보다", "이렇게", "카테고리리스트가", 
+  "많아지지는", "않겠다"
+];
+
 const MainHeader = () => {
 
   const [scrollTop, changeScrollTop] = useState(0);
+  const [selectedCategory, changeSelectCategory] = useState("ALL");
+  const [activePanel, changeActivePanel] = useState("");
 
   useEffect(() => {
     window.addEventListener('scroll', onScroll);
@@ -85,16 +97,21 @@ const MainHeader = () => {
   const onScroll = () => {
     const scrollTop = ('scroll', document.documentElement.scrollTop);
     changeScrollTop(scrollTop);
+    scrollTop > 20 ? changeActivePanel("") : changeActivePanel("category");
   }
   
   const category = (
+    <div>
     <span style={{
       fontSize: "12px", 
       fontWeight: "400", 
       color: "#666", 
       letterSpacing: '1px'}}>
         Category
-    </span>)
+    </span>&emsp;
+    {/* {selectedCategory !== "ALL" && <Tag color="#71bdb9">{selectedCategory}</Tag>} */}
+    {(!activePanel && selectedCategory !== "ALL") && <span style={{fontSize: "12px", color: "#71bdb9"}}>{selectedCategory}</span>}
+    </div>)
 
   return (
     <Container>
@@ -106,21 +123,20 @@ const MainHeader = () => {
       </HeaderWrapper>
       <CategoryWrapper>
         <CategoryArea>
-          <Collapse bordered={false} expandIconPosition="right" style={{width: "100%"}}>
-            <Panel header={category} style={{border: 0}}>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
-              <span style={{margin: "0 20px"}}>test</span>
+  <Collapse bordered={false} expandIcon={({ isActive }) => <Icon type="left" rotate={isActive ? -90 : 0} />} expandIconPosition="right" style={{width: "100%"}} activeKey={activePanel} onChange={(key) => {activePanel ? changeActivePanel("") : changeActivePanel(key)}}>
+            <Panel header={category} style={{border: 0}} key="category">
+              {tempCat.map((tag) => {
+                return (
+                  <CheckableTag
+                    key={tag}
+                    checked={(tag === selectedCategory) && true}
+                    onChange={checked => changeSelectCategory(tag)}
+                    style={{margin: "4px 12px 6px 0"}}
+                  >
+                    {tag}
+                  </CheckableTag>
+                )
+              })}
             </Panel>
           </Collapse>
           {/* <span style={{fontSize: "12px", fontWeight: "400", color: "#333", letterSpacing: '1px'}}>
